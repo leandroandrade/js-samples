45f3e089a79e5918868e77acede4bc8888513360
leandroandrade <leandro.andrade2588@gmail.com>
2021-02-08 20:20:05 -0300

    stubs

---
+++ stubs/src/service.test.js
@@ -0,0 +1,47 @@
+const sinon = require('sinon');
+const { deepStrictEqual } = require('assert');
+const Service = require('./service');
+
+const BASE_URL_1 = 'https://swapi.dev/api/planets/1/';
+const BASE_URL_2 = 'https://swapi.dev/api/planets/2/';
+
+const mocks = {
+    tatooine: require('../mocks/tatooine.json'),
+    alderaan: require('../mocks/alderaan.json'),
+};
+
+(async () => {
+    // {
+    //     /// internet
+    //     const service = new Service();
+    //     const response = await service.makeRequest(BASE_URL_2);
+    //     console.log(JSON.stringify(response));
+    // }
+
+    const service = new Service();
+
+    const stub = sinon.stub(service, service.makeRequest.name);
+    stub.withArgs(BASE_URL_1).resolves(mocks.tatooine);
+    stub.withArgs(BASE_URL_2).resolves(mocks.alderaan);
+
+    {
+        const mock = {
+            name: 'Tatooine',
+            surfaceWater: '1',
+            appearedIn: 5,
+        };
+
+        const result = await service.getPlanets(BASE_URL_1);
+        deepStrictEqual(result, mock);
+    }
+
+    {
+        const mock = {
+            name: 'Alderaan',
+            surfaceWater: '40',
+            appearedIn: 2,
+        };
+        const result = await service.getPlanets(BASE_URL_2);
+        deepStrictEqual(result, mock);
+    }
+})();
